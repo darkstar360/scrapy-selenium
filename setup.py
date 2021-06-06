33b8feac3f7701773a8ca01fccc454d223b5e42c
darkstar360 <avenger.reg@gmail.com>
2021-06-06 23:46:25 +0600

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 """This module contains the packaging routine for the pybook package"""
 
 from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+
 # try:
 #     from pip.download import PipSession
 #     from pip.req import parse_requirements
@@ -11,33 +13,33 @@ from setuptools import setup, find_packages
 #     from pip._internal.req.req_file import parse_requirements
 
 
-def get_requirements(source):
-    """Get the requirements from the given ``source``
-
-    Parameters
-    ----------
-    source: str
-        The filename containing the requirements
-
-    """
-
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
+# def get_requirements(source):
+#     """Get the requirements from the given ``source``
+#
+#     Parameters
+#     ----------
+#     source: str
+#         The filename containing the requirements
+#
+#     """
+#
+#     install_reqs = parse_requirements(filename=source, session='hack')
+#
+#     return [str(ir.req) for ir in install_reqs]
 
 
 setup(
-    packages=find_packages(),
-    install_requires=get_requirements('requirements/requirements.txt')
-    # name='scrapy-selenium',
-    # version='0.0.8',
-    # description='Scrapy with selenium',
-    # url='git@github.com:https://github.com/darkstar360/scrapy-selenium.git',
-    # author='darkstar',
+    # packages=find_packages(),
+    install_requires=['scrapy>=1.0.0','selenium>=3.9.0'],
+    name='scrapy-selenium',
+    version='0.0.8',
+    description='Scrapy with selenium',
+    url='git@github.com:https://github.com/darkstar360/scrapy-selenium.git',
+    author='darkstar',
     # author_email='raphael.schubert@digitalbankscorp.com',
-    # license='unlicense',
-    # packages=['scrapy_selenium'],
-    # zip_safe=False
+    license='unlicense',
+    packages=['scrapy_selenium'],
+    zip_safe=False
 )
 
 
